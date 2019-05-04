1aed49dd038e5477abfb12fb405d212851a85e91
NN <NN@PC15>
2019-05-04 18:10:56 +0700

    Init Project

--- EStoreTemplatev21/Startup.cs
+++ EStoreTemplatev21/Startup.cs
@@ -33,6 +33,8 @@ namespace EStoreTemplatev21
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MyeStoreContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("MyeStore")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
